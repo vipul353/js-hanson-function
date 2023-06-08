bc17b5a9461869a8d0df68eab50e446c48038835
vipul353 <thakurbipul353@gmail.com>
2023-06-08 09:03:15 +0530

    first commit

---
+++ sample.js
@@ -0,0 +1,9 @@
+const greet =  function(name){
+    return function(m){
+    
+        console.log(`Hi!! ${name}, ${m}`);
+    }
+}
+  
+const greet_message = greet('EA19');
+greet_message("Welcome To PrepBytes")
